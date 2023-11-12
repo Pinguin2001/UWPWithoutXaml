29e37b83c5825da64d5662c42a96afe3f9dbc40f
Julian Hasreiter <75797743+Pinguin2001@users.noreply.github.com>
2023-11-12 17:20:11 +0100

    Upload project files

---
+++ App.cs
@@ -0,0 +1,90 @@
+﻿using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPNoXaml
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            StackPanel pageRoot = Window.Current.Content as StackPanel;
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (pageRoot == null)
+            {
+                Button button = new Button
+                {
+                    Content = "Click me",
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                };
+                button.Click += Button_Click;
+
+                pageRoot = new StackPanel
+                {
+                    Orientation = Orientation.Vertical,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Children =
+                    {
+                        new TextBlock { Text = "Hello UWP without XAML!" },
+                        button
+                    }
+                };
+
+                // Place the frame in the current Window
+                Window.Current.Content = pageRoot;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (pageRoot.Children == null)
+                {
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
+                }
+                // Ensure the current window is active
+                Window.Current.Activate();
+            }
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            (sender as Button).Content = "Clicked!";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace UWPNoXaml
+{
+    public static class Program
+    {
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks", " 14.0.0.0")]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        static void Main(string[] args)
+        {
+            global::Windows.UI.Xaml.Application.Start((p) => new App());
+        }
+    }
+
+    partial class App : Windows.UI.Xaml.Application
+    {
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks", " 14.0.0.0")]
+        private bool _contentLoaded;
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.Windows.UI.Xaml.Build.Tasks", " 14.0.0.0")]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        public void InitializeComponent()
+        {
+            if (_contentLoaded)
+                return;
+
+            _contentLoaded = true;
+#if DEBUG && !DISABLE_XAML_GENERATED_BINDING_DEBUG_OUTPUT
+            DebugSettings.BindingFailed += (sender, args) =>
+            {
+                global::System.Diagnostics.Debug.WriteLine(args.Message);
+            };
+#endif
+#if DEBUG && !DISABLE_XAML_GENERATED_BREAK_ON_UNHANDLED_EXCEPTION
+            UnhandledException += (sender, e) =>
+            {
+                if (global::System.Diagnostics.Debugger.IsAttached) global::System.Diagnostics.Debugger.Break();
+            };
+#endif
+        }
+    }
+}
